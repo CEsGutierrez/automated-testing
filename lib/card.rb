6e9c1d4c067ca1faaec8ae565fb09466e6b8bda5
C Gutierrez <gutierrez.clara.es@gmail.com>
2019-08-26 10:15:59 -0700

    Wave 1 success

--- lib/card.rb
+++ lib/card.rb
@@ -3,16 +3,19 @@
 
 class Card
   attr_reader :value, :suit
-
+  
   def initialize(value, suit)
     @value = value
     @suit = suit
+    if value < 1 || value > 13
+      raise ArgumentError
+    end
     
-
+    
   end
-
+  
   def to_s
     return "#{value} of #{suit.to_s}"
   end
-
+  
 end
