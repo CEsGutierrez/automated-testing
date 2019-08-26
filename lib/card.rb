627ed30958e83ae7a00d0585eea44600e3834cb0
C Gutierrez <gutierrez.clara.es@gmail.com>
2019-08-26 10:30:23 -0700

    Wave 2 success

--- lib/card.rb
+++ lib/card.rb
@@ -15,7 +15,12 @@ class Card
   end
   
   def to_s
-    return "#{value} of #{suit.to_s}"
+    references = {1 => "Ace", 11 => "Jack", 12 => "Queen", 13 => "King"}
+    if references.has_key?(value)
+      return "#{references.fetch(value)} of #{suit.to_s}"
+    else 
+      return "#{value} of #{suit.to_s}"
+    end
+    
   end
-  
 end
--- test/card_test.rb
+++ test/card_test.rb
@@ -2,7 +2,7 @@ require_relative 'test_helper'
 
 describe Card do
   describe "You can create a Card instance" do
-
+    
     it "Can be created" do
       [:hearts, :spades, :clubs, :diamonds].each do |suit|
         (1..13).each do |value|
@@ -11,7 +11,7 @@ describe Card do
         end
       end
     end
-
+    
     it "Raises an ArgumentError for invalid parameters" do
       # To raise an ArgumentError in card.rb
       # raise ArgumentError.new
@@ -20,15 +20,27 @@ describe Card do
       expect {Card.new(14, :clovers)}.must_raise ArgumentError
     end
   end
-
+  
   describe "Testing to_s" do
-
+    
     it "to_s returns a readable String value logically for values 2-10" do
+      
+      expected_statement = "5 of hearts"
+      
+      card = Card.new(5, :hearts)
+      expect card.to_s.must_equal expected_statement
+      expect card.to_s.must_be_instance_of String
       # Test to ensure that to_s works for cards values 2-10
       # for example:  "2 of diamonds"
     end
-
+    
     it "to_s returns a readable String value for Ace, Jack, Queen, King" do
+      
+      expected_statement = "King of diamonds"
+      card = Card.new(13, :diamonds)
+      expect card.to_s.must_equal expected_statement
+      expect card.to_s.must_be_instance_of String
+      
       # Test to ensure that to_s works for cards values 1, and 11-13
       # For example: "Queen of hearts"
       # The current implementation of to_s does not address this feature
@@ -36,17 +48,17 @@ describe Card do
       # (Consider writing a helper method!)
     end
   end
-
+  
   describe "Reader methods" do
-
+    
     it "Can retrieve the value of the card using a `.value`." do
       # ensure that `.value works as expected`
     end
-
+    
     it "Can retrieve the value of the card using a `.suit`." do
       # ensure that `.suit works as expected returning the symbol of the suit`
-
+      
     end
   end
-
+  
 end
